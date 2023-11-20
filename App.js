9d23c80064a16b2a010efa9d00deb26b45354464
DimaLedentsov <ledentd@mail.ru>
2023-11-20 19:29:43 +0300

    сделал

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+    const [input, setInput] = useState('');
+    const clearInput = () => {
+        setLogin('');
+        setPassword('');
+    }
+
+    const updateInput = () => {
+      setInput(`login: ${login} password: ${password}`);
+    }
+
+    return (
+        <View style={styles.loginForm}>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={newText => setLogin(newText)}
+                    placeholder='Login'
+                    defaultValue={login}
+                    />
+                <TextInput
+                secureTextEntry={true}
+                    style={styles.input}
+                    onChangeText={newText => setPassword(newText)}
+                    placeholder='Password'
+                    defaultValue={password}
+                    />
+                
+            </View>
+            <View style={styles.buttonContainer}>
+                <Button 
+                    title='Clear'
+                    onPress={clearInput}
+                    />
+                <Button 
+                    title='Login'
+                    onPress={updateInput}
+                    />
+            </View>
+            <Text>{input}</Text>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    loginForm: {
+        paddingTop: 50,
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        rowGap: 5,
+    },
+    inputContainer: {
+        flexDirection: 'column',
+        width: '70%',
+        rowGap: 10,
+    },
+    input: {
+        borderWidth: 1,
+        borderRadius: 2,
+        padding: 5,
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        columnGap: 5,
+    },
 });
