b853183ba165c75b3ee5b1dbf8102b8ab413f110
DimaLedentsov <ledentd@mail.ru>
2023-12-01 19:09:11 +0300

    .

--- components/Item.jsx
+++ components/Item.jsx
@@ -2,13 +2,13 @@ import {TouchableOpacity, Alert} from 'react-native';
 import styled from 'styled-components/native';
 import { useNavigation } from '@react-navigation/native';
 const Image = styled.Image`
-    width: 100%;
-    height: 100px;
+    aspectRatio: 1.5;
+    width:100%;
     resizeMode: cover;
     margin-bottom: 20px;
 `
 const Container = styled.View`
-
+    flex:1; 
 `
 
 const Title = styled.Text`
